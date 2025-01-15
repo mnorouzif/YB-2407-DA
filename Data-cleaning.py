7db8abee3db8c31ad9aacd4acf7c9f1c53e2bfd7
Mohammad <mnorouzif@gmail.com>
2025-01-15 16:04:50 +1300

    initial update
    Minor changes

---
+++ Data-cleaning.py
@@ -0,0 +1,15 @@
+from sklearn import datasets
+import pandas as pd
+
+# load iris dataset
+iris = datasets.load_iris()
+# Since this is a bunch, create a dataframe
+iris_df=pd.DataFrame(iris.data)
+iris_df['class']=iris.target
+
+iris_df.columns=['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid', 'class']
+iris_df.dropna(how="all", inplace=True) # remove any empty lines
+
+#selecting only first 4 columns as they are the independent(X) variable
+# any kind of feature selection or correlation analysis should be first done on these
+iris_X=iris_df.iloc[:,[0,1,2,3]]
