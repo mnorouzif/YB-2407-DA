c7eac636b17d90e74621989091d522ab5616705e
Mohammad <mnorouzif@gmail.com>
2025-01-23 15:00:51 +1300

    initial changes v0.1

--- Week6 - Iris dataset cleaning and correlation/Data-cleaning.py
+++ Week6 - Iris dataset cleaning and correlation/Data-cleaning.py
@@ -11,6 +11,8 @@ iris_df['class']=iris.target
 
 iris_df.columns=['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid', 'class']
 #### ===> TASK 1: here - add two more lines of the code to find the number and mean of missing data
+#missing value
+
 cleaned_data = iris_df.dropna(how="all", inplace=True) # remove any empty lines
 
 
@@ -18,3 +20,6 @@ iris_X=iris_df.iloc[:5,[0,1,2,3]]
 print(iris_X)
 
 ### TASK2: Here - Write a short readme to explain above code and how we can calculate the corrolation amoung featuers with description
+# Task 3: Find Dublicated recordes and drop them
+# Task 4 : outliers detection - (week 8)
+# ...
